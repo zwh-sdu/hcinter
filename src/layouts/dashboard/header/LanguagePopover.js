056c1c28e80bc6480bc9ce936b4800ad0cfd34e4
zwh-sdu <386348499z@gmail.com>
2022-10-19 19:49:11 +0800

    init

--- src/layouts/dashboard/header/LanguagePopover.js
+++ src/layouts/dashboard/header/LanguagePopover.js
@@ -9,17 +9,17 @@ const LANGS = [
   {
     value: 'en',
     label: 'English',
-    icon: '/assets/icons/ic_flag_en.svg',
+    icon: '%PUBLIC_URL%/assets/icons/ic_flag_en.svg',
   },
   {
     value: 'de',
     label: 'German',
-    icon: '/assets/icons/ic_flag_de.svg',
+    icon: '%PUBLIC_URL%/assets/icons/ic_flag_de.svg',
   },
   {
     value: 'fr',
     label: 'French',
-    icon: '/assets/icons/ic_flag_fr.svg',
+    icon: '%PUBLIC_URL%/assets/icons/ic_flag_fr.svg',
   },
 ];
 
